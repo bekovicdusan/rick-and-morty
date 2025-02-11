4a81f40cc0c5e3ce7be1e4001534c9593045f6d3
bekovicdusan <bekovicdusan94@gmail.com>
2025-02-11 15:08:06 +0100

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div class="min-h-screen bg-gray-900 text-white">
+    <Header />
     <router-view />
   </div>
 </template>
+
+<script setup lang="ts">
+import Header from "./components/Header.vue"
+</script>
 
 <style scoped>
 .logo {
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="bg-gray-600 p-4 text-white">
+    <div class="container mx-auto flex justify-between items-center">
+      <h1 class="text-2xl font-bold">
+        <router-link to="/" class="text-white hover:text-gray-400">
+          Rick and Morty
+        </router-link>
+      </h1>
+      <nav>
+        <router-link to="/" class="mr-6 text-white hover:text-gray-400">
+          Characters
+        </router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+header {
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
