f05bd19aae8c90e54fb7434878f989ba6131c952
bekovicdusan <bekovicdusan94@gmail.com>
2025-02-11 15:09:07 +0100

    fix: integrate store action

--- src/views/CharacterView.vue
+++ src/views/CharacterView.vue
@@ -1,10 +1,12 @@
 <template>
-  <div v-if="character" class="p-6">
-    <button @click="$router.back()" class="mb-4 text-blue-500">← Back</button>
+  <div v-if="isLoading" class="p-6">Loading...</div>
+  <div v-else-if="error" class="p-6 text-red-500">{{ error }}</div>
+
+  <div v-else-if="character" class="p-6">
     <div class="bg-gray-800 p-6 rounded-lg flex items-center">
       <img :src="character.image" :alt="character.name" class="rounded-lg w-48 h-48" />
       <div class="ml-6">
-        <h1 class="text-3xl font-bold">{{ character.name }}</h1>
+        <h1 class="text-3xl font-bold mb-2">{{ character.name }}</h1>
         <p class="text-lg"><strong>Status:</strong> {{ character.status }}</p>
         <p class="text-lg"><strong>Species:</strong> {{ character.species }}</p>
         <p class="text-lg"><strong>Origin:</strong> {{ character.origin.name }}</p>
@@ -14,17 +16,19 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
 import { useRoute } from "vue-router";
-import { useCharacterStore } from "../store/character.store"
+import { useCharacterStore } from "../store/character.store";
 
 const route = useRoute();
-const character = ref<any>(null);
+
 const store = useCharacterStore();
 
+const { character, isLoading, error } = storeToRefs(store);
+
 onMounted(async () => {
-  const res = await fetch(`https://rickandmortyapi.com/api/character/${route.params.id}`);
-  character.value = await res.json();
+  store.fetchCharacterById(route.params.id as string);
 });
 </script>
 
