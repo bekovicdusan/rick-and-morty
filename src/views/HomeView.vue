aed463d7336cf374b38d80d7f7e10d904aab12c5
bekovicdusan <bekovicdusan94@gmail.com>
2025-02-11 15:09:16 +0100

    fix: ui adjustments

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="p-6">
+  <div class="p-6 flex flex-col items-center">
     <h1 class="text-2xl font-bold mb-4">
       Rick and Morty Characters ({{ characterCount }})
     </h1>
 
     <p v-if="isLoading">Loading...</p>
 
-    <div v-else class="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <div v-else class="flex flex-col">
       <div v-for="character in characters" :key="character.id"
-        class="bg-gray-800 p-4 rounded-lg cursor-pointer hover:bg-gray-700"
+        class="bg-gray-800 p-4 rounded-lg cursor-pointer hover:bg-gray-700 m-2"
         @click="router.push(`/character/${character.id}`)">
         <img :src="character.image" :alt="character.name" class="rounded-lg" />
         <h2 class="text-lg font-semibold mt-2">{{ character.name }}</h2>
@@ -28,6 +28,6 @@ const { characters, isLoading, characterCount } = storeToRefs(characterStore);
 const router = useRouter();
 
 onMounted(async () => {
-  await characterStore.value.fetchCharacters();
+  await characterStore.fetchCharacters();
 })
 </script>
