4ab1c81fa2f848e56d3e21e84dc92bab26d805c9
bekovicdusan <bekovicdusan94@gmail.com>
2025-02-11 15:35:20 +0100

    feat: add caching

--- src/store/character.store.ts
+++ src/store/character.store.ts
@@ -7,15 +7,27 @@ export const useCharacterStore = defineStore("character", () => {
   const characters = ref<any[]>([]);
   const isLoading = ref(false);
   const error = ref<string | null>(null);
+  const cachedData = ref<Record<string, any>>({});
 
   const characterCount = computed(() => characters.value.length);
 
-  const fetchCharacters = async () => {
-    isLoading.value = true;
+  const fetchCharacters = async (page: number = 1) => {
     error.value = null;
+    const cacheKey = `characters-page-${page}`;
+
+    // check if data cached
+    if (cachedData.value[cacheKey]) {
+      characters.value = cachedData.value[cacheKey];
+      return;
+    }
+
+    isLoading.value = true;
 
     try {
-      characters.value = await getCharacters();
+      characters.value = await getCharacters(page);
+
+      // cache fetched data
+      cachedData.value[cacheKey] = characters.value;
     } catch (err) {
       error.value = "Failed to fetch characters";
       console.error(err);
@@ -25,10 +37,20 @@ export const useCharacterStore = defineStore("character", () => {
   };
 
   const fetchCharacterById = async (id: string | number) => {
-    isLoading.value = true;
     error.value = null;
+    const cacheKey = `character-${id}`;
+
+    if (cachedData.value[cacheKey]) {
+      character.value = cachedData.value[cacheKey];
+      return;
+    }
+
+    isLoading.value = true;
+
     try {
       character.value = await getCharacterById(id);
+
+      cachedData.value[cacheKey] = character.value;
     } catch (err) {
       error.value = "Failed to fetch character details";
       console.error(err);
@@ -36,8 +58,6 @@ export const useCharacterStore = defineStore("character", () => {
       isLoading.value = false;
     }
   };
-
-  // onMounted(fetchCharacters); // Auto-fetch characters on mount
 
   return {
     character,
