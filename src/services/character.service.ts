45ef779de0e509dd6f3d8bc389ff9a1d28678b83
bekovicdusan <bekovicdusan94@gmail.com>
2025-02-11 14:05:24 +0100

    feat: add axios and service

---
+++ src/services/character.service.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "https://rickandmortyapi.com/api",
+  timeout: 5000,
+});
+
+export const getCharacters = async () => {
+  try {
+    const { data } = await apiClient.get("/character");
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching characters: ", error);
+    throw error;
+  }
+};
+
+export const getCharacterById = async (id: string | number) => {
+  try {
+    const { data } = await apiClient.get(`/character/${id}`);
+    return data;
+  } catch (error) {
+    console.error(`Error fetching character ${id}:`, error);
+    throw error;
+  }
+};
