a423071e00e25d6430ba2623b607d778bbb7ce99
bekovicdusan <bekovicdusan94@gmail.com>
2025-02-12 15:01:44 +0100

    feat: add search (filtering)

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full flex items-center relative">
+    <input v-model="searchQuery" class="w-full px-2 py-1" type="text" placeholder="Search by character name"
+      @input="emitSearch" />
+    <font-awesome-icon icon="fa-solid fa-search" class="absolute right-4" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { debounce } from "lodash";
+
+const emit = defineEmits<{
+  "update:search": [query: string]
+}>()
+
+const searchQuery = ref<string>("");
+
+const emitSearch = debounce(() => {
+  emit("update:search", searchQuery.value)
+}, 200);
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,48 @@
 <template>
   <div class="p-6 flex flex-col items-center">
     <h1 class="text-2xl font-bold mb-4">
-      Rick and Morty Characters ({{ characterCount }})
+      Rick and Morty Characters ({{ filteredCharacters.length }})
     </h1>
 
-    <p v-if="!characters.length">Loading...</p>
+    <p v-if="!characters.length && !hasReachedTheEnd">Loading...</p>
 
     <div v-else class="flex flex-col" ref="characterList">
-      <div v-for="character in characters" :key="character.id"
-        class="bg-gray-800 p-4 rounded-lg cursor-pointer hover:bg-gray-700 m-2"
+      <SearchBox class="p-2" v-model:search="searchQuery" />
+      <div v-for="character in filteredCharacters" :key="character.id"
+        class="bg-gray-800 p-4 rounded-lg cursor-pointer hover:bg-gray-700 m-2 max-w-[322px]"
         @click="router.push(`/characters/${character.id}`)">
         <img :src="character.image" :alt="character.name" class="rounded-lg" />
         <h2 class="text-lg font-semibold mt-2">{{ character.name }}</h2>
       </div>
+      <p v-if="hasReachedTheEnd" class="flex items-center justify-center w-full mt-2 text-lg font-medium">That's all
+        folks!</p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from "vue";
+import { onMounted, onUnmounted, ref, computed } from "vue";
 import { useCharacterStore } from '../store/character.store';
 import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
 
+import SearchBox from "../components/SearchBox.vue";
+
 const router = useRouter();
 
 const characterStore = useCharacterStore();
-const { characters, characterCount } = storeToRefs(characterStore);
+const { characters } = storeToRefs(characterStore);
 
 const characterList = ref<HTMLElement | null>(null);
 const page = ref<number>(1);
+const searchQuery = ref<string>("");
+const hasReachedTheEnd = ref<boolean>(false);
+
+const filteredCharacters = computed(() => {
+  return characters.value.filter((character) => {
+    return character.name.toLowerCase().includes(searchQuery.value.toLowerCase());
+  });
+});
 
 onMounted(async () => {
   window.addEventListener("scroll", handleScroll);
@@ -50,6 +63,11 @@ const handleScroll = async () => {
 
 const loadMoreCharacters = async () => {
   page.value += 1;
-  await characterStore.fetchCharacters(page.value);
+  // there are 42 pages in total
+  if (page.value < 43) {
+    await characterStore.fetchCharacters(page.value);
+  } else {
+    hasReachedTheEnd.value = true;
+  }
 };
 </script>
