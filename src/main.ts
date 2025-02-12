944e640cbc3b95ba201800aa1e17aa097e8d3f8c
bekovicdusan <bekovicdusan94@gmail.com>
2025-02-12 14:58:30 +0100

    feat: add lodash and font-awesome

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { createApp } from "vue";
 import "./style.css";
 import router from "./router";
 import { createPinia } from "pinia";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import App from "./App.vue";
+
+library.add(faSearch);
 
 const app = createApp(App);
 app.use(router);
 app.use(createPinia());
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
